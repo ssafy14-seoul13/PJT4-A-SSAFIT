1b22287b9f1ea232a430f5c6c3294bb4c7b89ee0
jeongns2611 <jeongns2611@gmail.com>
2025-09-26 17:06:30 +0900

    feat: 전역 에러 처리를 위한 설정 및  jsp 추가, ErrorTest 전용 ErrorTestServlet 추가

---
+++ src/main/java/com/ssafy/ssafit/controller/ErrorTestServlet.java
@@ -0,0 +1,20 @@
+package main.java.com.ssafy.ssafit.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/errorTest")
+public class ErrorTestServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		throw new RuntimeException();
+	}
+}
--- src/main/webapp/WEB-INF/common/error.jsp
+++ src/main/webapp/WEB-INF/common/error.jsp
@@ -1,12 +1,13 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
+<%@ page isErrorPage="true" language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Error Page</title>
 </head>
 <body>
-
+	<h1>에러 발생!!!!</h1>
 </body>
 </html>
