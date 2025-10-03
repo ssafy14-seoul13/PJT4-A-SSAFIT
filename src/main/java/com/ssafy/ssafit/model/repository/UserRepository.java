0e345eb193ce892acedcd065b2605ab548640d57
jeongns2611 <jeongns2611@gmail.com>
2025-10-03 17:04:38 +0900

    refactor: UserRepositoryImpl을 메모리 기반에서 JDBC 기반 DB 연동으로 변경 (save(), findById(), findAll() 메서드 수정)

--- src/main/java/com/ssafy/ssafit/model/repository/UserRepository.java
+++ src/main/java/com/ssafy/ssafit/model/repository/UserRepository.java
@@ -5,7 +5,8 @@ import java.util.List;
 import com.ssafy.ssafit.model.dto.User;
 
 public interface UserRepository {
-    void save(User user);
+    User save(User user);
+    void saveUserVideos(int userNo, List<Integer> videoNos);
     User findById(String id);
     List<User> findAll();
     boolean update(User user);
